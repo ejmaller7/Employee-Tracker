4cb301d717dc4ba7bafde4637ee6875814675de6
Ethan Maller <mallerethan4@gmail.com>
2024-12-04 00:31:51 -0600

    Fifth commit - wrote code to allow adding new department, role and employee

---
+++ db/addToTables.sql
@@ -0,0 +1,11 @@
+-- This is to add a new department
+INSERT INTO Department (name)
+    VALUES ('');
+
+-- This is to add a new role
+INSERT INTO Role (title, salary, department_id)
+    VALUES ('', , );
+
+-- This is to add a new employee
+INSERT INTO Employee (first_name, last_name, role_id, manager_id)
+    VALUES ('', '', , );
