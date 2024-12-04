079988cab3d4c759b1b40dc82178e09ed3fadd58
Ethan Maller <mallerethan4@gmail.com>
2024-12-03 23:40:16 -0600

    Third commit - Writing the code for update.sql and delete.sql as well as providing an image for what the tables look like

---
+++ db/delete.sql
@@ -0,0 +1,8 @@
+-- This would be to delete a department
+DELETE FROM Department where id = ;
+
+-- This would be to delete a role
+DELETE FROM Role where id = ;
+
+-- This would be to delete an employee
+DELETE FROM Employee where id = ;
---
+++ db/update.sql
@@ -0,0 +1,15 @@
+-- This would be used to update a department name if needed
+UPDATE Department SET name = '' where id = ;
+
+-- This would be used to update a role if needed
+UPDATE Role SET 
+    title = '',
+    salary = 
+where id = ;
+
+-- This would be to update an employee if needed
+UPDATE Employee SET 
+    first_name = '', 
+    last_name = '', 
+    manager_id =  
+where id = ;
