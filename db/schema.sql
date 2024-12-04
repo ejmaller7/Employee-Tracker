9e4d43c8108775983df9e4a6d8d0c63cfd91a974
Ethan Maller <mallerethan4@gmail.com>
2024-12-03 23:19:23 -0600

    First commit - Writing the schema.sql and seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+SELECT 'Database rebuild started...';
+DROP DATABASE IF EXISTS temp;
+CREATE DATABASE temp;
+
+\c temp;
+
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'employeeTracker_db'
+  AND pid <> pg_backend_pid();
+
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+\c employeeTracker_db;
+
+CREATE TABLE Department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE Role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    deparment_id INTEGER NOT NULL
+    FOREIGN KEY (deparment_id) REFERENCES Department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE Employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
+    FOREIGN KEY (role_id) REFERENCES Role(id) ON DELETE SET NULL
+);
